99e51fdb25db1ab4f88d76d2f42fd8e930330562
AzuxDario <pawel_mibor95@tlen.pl>
2020-08-17 15:40:42 +0200

    Add dark style

--- Image-Duplicate-Finder/main.cpp
+++ Image-Duplicate-Finder/main.cpp
@@ -1,10 +1,31 @@
 #include "Widget.h"
 
 #include <QApplication>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    qApp->setStyle(QStyleFactory::create("Fusion"));
+    QPalette p = qApp->palette();
+    p.setColor(QPalette::Window, QColor(53,53,53));
+    p.setColor(QPalette::WindowText, Qt::white);
+    p.setColor(QPalette::Base, QColor(15,15,15));
+    p.setColor(QPalette::AlternateBase, QColor(53,53,53));
+    p.setColor(QPalette::ToolTipBase, Qt::white);
+    p.setColor(QPalette::ToolTipText, Qt::white);
+    p.setColor(QPalette::Text, Qt::white);
+    p.setColor(QPalette::Button, QColor(53,53,53));
+    p.setColor(QPalette::ButtonText, Qt::white);
+    p.setColor(QPalette::BrightText, Qt::red);
+    p.setColor(QPalette::Highlight, QColor(00,102,204).lighter());
+    p.setColor(QPalette::HighlightedText, Qt::black);
+
+    p.setColor(QPalette::Disabled, QPalette::Base, QColor(25,25,25));
+    p.setColor(QPalette::Disabled, QPalette::Text, Qt::darkGray);
+    p.setColor(QPalette::Disabled, QPalette::Button, QColor(43,43,43));
+    p.setColor(QPalette::Disabled, QPalette::ButtonText, Qt::darkGray);
+    qApp->setPalette(p);
     Widget w;
     w.show();
     return a.exec();
